3ebb79e4932389a6cdd660aa40367ed71252d0b4
nlyapich <example@mail.com>
2025-11-25 18:09:19 +0700

    add new file

---
+++ src/main/java/ru/tcai/taskservice/dto/request/CommentRequest.java
@@ -0,0 +1,17 @@
+package ru.tcai.taskservice.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentRequest {
+    private Long authorId;
+    private String text;
+}
